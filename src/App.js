981eb2f6f0fb9bab9a5d57eb513aec13a1f36a2a
RG-67 <rittikghoshkolkata@gmail.com>
2025-10-10 09:50:00 +0530

    updated 10th Oct, 2025

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
           Full Stack Developer | Node.js | React Native | PostgreSQL | Android
         </p>
         <a
-          href="https://drive.google.com/uc?export=download&id=1OIdRf1TCOkGmH2Xb-_SVVgrrcgBwqIHb"
+          href="https://drive.google.com/uc?export=download&id=1AzXbpRXFzYh8A6PUlLUYh1fsmCIaKcps"
           download
           className="inline-block mt-6 bg-blue-500 hover:bg-blue-600 text-white px-5 py-2 rounded-md transition"
         >
